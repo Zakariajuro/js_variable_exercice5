07060b6eca71622bdcf8a1fb81993bd8eb276a6e
Zakaria <zakPro@live.be>
2020-12-28 17:05:14 +0100

    exo-fini

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+// EXERCICE 1
+// let cinq = 5
+// console.log(5);
+
+// let dix = 10
+// console.log(10);
+
+// // addition 
+// let result = cinq + dix
+// console.log(`${cinq} + ${dix} = ${result}`);
+
+// // soustraction 
+// let result2 = dix - cinq
+// console.log(`${cinq} - ${dix} = ${result2}`);
+
+// // multiplication
+// let result3 = dix * cinq
+// console.log(`${cinq} * ${dix} = ${result3}`);
+
+// // division 
+// let result4 = dix / cinq
+// console.log(`${cinq} / ${dix} = ${result4}`);
+
+// EXERCICE 2 
+
+let nom = prompt('votre prenom ?')
+console.log(nom);
+
+let age = prompt('votre âge ?')
+console.log(age);
+
+console.log(`${nom} a ${age} cette année `);
+
+console.log(`${nom} aura ${age} dans 3 ans`);
